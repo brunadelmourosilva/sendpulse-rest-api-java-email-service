acf91e0ec981ee18ccece74f72be0f88b1b6d135
Bruna Delmouro <brunadelmouro@gmail.com>
2022-02-25 16:05:51 -0300

    update

--- sendpulse-rest-api-java-email-service/src/com/sendpulse/ExampleJobs.java
+++ sendpulse-rest-api-java-email-service/src/com/sendpulse/ExampleJobs.java
@@ -1,17 +1,12 @@
 package com.sendpulse;
 
 import com.sendpulse.restapi.JsonTools;
-import com.sendpulse.restapi.PersonalData;
 import com.sendpulse.restapi.Sendpulse;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Scanner;
 
 /*
  * Estágio Online
@@ -19,28 +14,14 @@ import java.util.List;
 
 public class ExampleJobs {
 
+    static final Scanner SC = new Scanner(System.in);
+
     // https://login.sendpulse.com/settings/#api
     private static String userId = "2be7d28fc637441d8358c940a8df871b"; // **insert ID**
     private static String secret = "0cf8894c2a35732ce8ee9b7b336f5a94"; // **insert secret**
 
     public static void main(String[] args) {
         Sendpulse sendpulse = new Sendpulse(userId, secret);
-
-        /* Creating and adding data by CSV */
-        List<PersonalData> listCSV = new ArrayList<>();
-
-        String line = "";
-        String splitBy = ",";
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("CSVTest.csv"));
-            while ((line = br.readLine()) != null) {
-                String[] data = line.split(splitBy);
-
-                listCSV.add(new PersonalData(data[0], data[1], data[2])); //email, cidade and curso
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         /* Creating a Mailing List */
         /* Get list_id by JSON format */
@@ -52,12 +33,10 @@ public class ExampleJobs {
 
 
         String listId = addressBook.toString(2).substring(19, 27); //get a specific id
+
         System.out.println("bookName: " + "contatos[" + sdf.format(now) + "]");
         System.out.println("listId: " + listId + "\n");
 
-
-        /* Creating a JSON format to add the emails and variables */
-        //StringBuilder listByJson = listEmailsJsonFormat(listCSV);
 
         JsonTools jt = new JsonTools();
         String json = null;
@@ -74,8 +53,6 @@ public class ExampleJobs {
 
         System.out.println("----------------------------------------------------------------------------");
 
-
-
         /* Add a time sleep between mailing list and campaign */
         try {
             Thread.sleep(300000); //5 minutes
@@ -83,66 +60,33 @@ public class ExampleJobs {
             e.printStackTrace();
         }
 
+        System.out.println("----------------------------------------------------------------------------");
+
+        System.out.print("Título do e-mail: ");
+        String emailTitle = SC.nextLine();
+
+        //adicionar template - listar templates
+
+        System.out.print("Nome da campanha: ");
+        String campaignName = SC.nextLine();
 
         System.out.println("----------------------------------------------------------------------------");
 
         //templateId: 1656009
         //bookId: 89398056
         /* Creating and sending a campaign */
-        /**
-         * Sending email via SendPulse Email Service
-         * @param senderName
-         * @param senderEmail
-         * @param subject
-         * @param templateId
-         * @param listId
-         * @param name
-         * @param attachments
-         */
         System.out.println(
                 sendpulse.createCampaign("Estágio Online",
                         "contato@estagioonline.com",
-                        "Empresas abriram vagas em sua região e área de atuação",
+                        emailTitle,
                         1656009,
-                        89398056,
-                        "VAGAS - 1498 usuários",
+                        Integer.parseInt(listId),
+                        campaignName,
                         "")
         );
 
         System.out.println("Campaign created!");
         System.out.println("----------------------------------------------------------------------------");
 
-    }
-
-    public static StringBuilder listEmailsJsonFormat(List<PersonalData> listCSV) {
-        StringBuilder listEmailsJsonFormat = new StringBuilder();
-
-        //ADICIONAR VAGA 1, VAGA 2, VAGA 3
-        listEmailsJsonFormat.append("[");
-        for (int j = 0; j < listCSV.size(); j++) {
-            listEmailsJsonFormat.append("{"
-                    + "\"email\":\"" + listCSV.get(j).getEmail() + "\","
-                    + "\"variables\":{"
-                    + "\"cidade\":\"" + listCSV.get(j).getCidade() + "\","
-                    + "\"curso\":\"" + listCSV.get(j).getCurso() + "\""
-                    + "}"
-                    + "},");
-        }
-        listEmailsJsonFormat.append("]");
-
-
-        /* Removing the last comma */
-        for (int i = listEmailsJsonFormat.toString().length() - 1; i >= 0; i--) {
-            //System.out.println(i);
-            if (listEmailsJsonFormat.charAt(i) == ',') {
-                listEmailsJsonFormat.setCharAt(i, ' ');
-                //System.out.println("OK");
-                break;
-            } else {
-                continue;
-            }
-        }
-
-        return listEmailsJsonFormat;
     }
 }
